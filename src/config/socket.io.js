abeb44dada8ff6e0882f1b122bc128c9f52f9b82
Reimon <ramon.fuentes.alarcon@gmail.com>
2021-08-12 23:24:38 +0100

    Socket
    Socket fixed

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_API_URL);
+const socket = io(process.env.REACT_APP_API_URL, {transports: ['websocket', 'polling', 'flashsocket']});
 
 export default socket;
