44b75acc6fa501270855c2719b2eb24781ff883d
Reimon <ramon.fuentes.alarcon@gmail.com>
2021-08-13 16:34:51 +0100

    Socket.io v2
    Socket.io v2 done

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -2,18 +2,4 @@ import io from 'socket.io-client';
 
 const socket = io(process.env.REACT_APP_SOCKET_URL);
 
-console.log("process.env.REACT_APP_SOCKET_URL: ", process.env.REACT_APP_SOCKET_URL);
-console.log("process.env.REACT_APP_API_URL: ", process.env.REACT_APP_API_URL);
-
-// const socket = io(process.env.REACT_APP_API_URL, {
-//   withCredentials: true,
-//   transportOptions: {
-//     polling: {
-//       extraHeaders: {
-//         "my-custom-header": "reimon"
-//       }
-//     }
-//   }
-// });
-
 export default socket;
