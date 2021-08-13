e69acce831a975b5948e7a6fd084a836fcff78ba
Reimon <ramon.fuentes.alarcon@gmail.com>
2021-08-13 14:07:19 +0100

    Socket.io
    Socket.io fixed

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_API_URL);
+const socket = io(process.env.REACT_SOCKET_API_URL);
 // const socket = io("https://www.rfsoftdev.xyz/");
 
 // const socket = io(process.env.REACT_APP_API_URL, {
