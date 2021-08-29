6a774f1750ae1f143f7713aa12dd9c2912f8c659
Reimon <ramon.fuentes.alarcon@gmail.com>
2021-08-29 22:13:21 +0100

    Socket.io
    Socket.io done

--- src/config/socket.io.js
+++ src/config/socket.io.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.REACT_APP_SOCKET_URL, {path: '/wea/'});
+const socket = io(process.env.REACT_APP_SOCKET_URL, {path: '/socket.io'});
 
 export default socket;
